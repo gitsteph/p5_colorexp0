b7809022abe04ef0b0bbfd36d53c18abf088797d
Stephanie H. Andrews <byronic.string@gmail.com>
2018-09-04 18:00:40 -0700

    Adding more GUI stuff, and started a README doc

---
+++ README.md
@@ -0,0 +1,7 @@
+# p5_colorexp0: Another Sound + Color Experiment
+
+## Notes
+
+I'll articulate more in this doc later.
+
+For now I'm adding this to credit [@jimmyland](https://github.com/jimmyland) for helping me talk through various implementations (+ debug some javascript weirdness).  Apparently JavaScript indices are strings (of integers), rather than integers.  Pretty strange stuff...
--- sketch.js
+++ sketch.js
@@ -1,142 +1,157 @@
 var capture;
 var img;
 var pixelCount;
-var colors;
-var first_capture;
+var colortonesArray;
+var noteFreqObj;
 
 const detectionDiffInt = 800;
 
 
 function setup() {
-    colors = {
-        'red': {
-            'rgb': [209, 0, 0],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        },
-        'orange': {
-            'rgb': [255, 102, 34],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        },
-        'yellow': {
-            'rgb': [255, 218, 33],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        },
-        'green': {
-            'rgb': [51, 221, 0],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        },
-        'blue': {
-            'rgb': [17, 51, 204],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        },
-        'purple': {
-            'rgb': [51, 0, 68],
-            'frequency': null,
-            'firstCount': null,
-            'lastCount': 0,
-            'currentCount': 0
-        }
+    // setting the scale, default middle C
+    // x2 to go up or down an octave
+    // TODO: enable octave selection
+    noteFreqObj = {
+        "A": 220.00,
+        "A#": 233.08,
+        "B": 246.94,
+        "C": 261.63,
+        "C#": 277.18,
+        "D": 293.66,
+        "D#": 311.13,
+        "E": 329.63,
+        "F": 349.23,
+        "F#": 369.99,
+        "G": 392.00,
+        "G#": 415.30
     }
 
-    var tempCounter = 0; // remove this; replace with some enumerate equivalent
-    for (var colorKey in colors) {
-        // instantiate oscillator
-        var env = new p5.Envelope();
-        env.setADSR(.001, .2, .2, .5);
-        env.setRange(0.4, 0.0);
-        var osc = new p5.Oscillator();
-        osc.setType('sine');
-        var freq = 200 + tempCounter * (800 / Object.keys(colors).length);
-        osc.freq(freq);
-        osc.amp(env);
-        osc.start();
-        colors[colorKey]['env'] = env;
-        tempCounter += 1;
-    }
+    colortonesArray = new Array();
 
-    var targetVideoWidth = windowWidth * 0.50;
-    var targetVideoHeight = windowHeight * 0.50;
+    let targetVideoWidth = windowWidth * 0.80;
+    let targetVideoHeight = windowHeight * 0.80;
 
     capture = createCapture(VIDEO);
     capture.size(targetVideoWidth, targetVideoHeight);
     capture.hide();
+    pixelCount = 4 * capture.width * capture.height;
 
-    var pixelD = pixelDensity();
-    pixelCount = 4 * (capture.width * pixelD) * (capture.height * pixelD);
-    first_capture = true;
+    var canvas = createCanvas(windowWidth * 0.96, windowHeight * 0.96);
+    background("lightblue");
 }
 
-function calculateClosestColors() {
-    for (var colorKey in colors) {
-        colors[colorKey]['lastCount'] = colors[colorKey]['currentCount'];
-        colors[colorKey]['currentCount'] = 0;
-    }
+// TODO: rework this function
+// function calculateClosestColors() {
+//     for (var colorKey in colors) {
+//         colors[colorKey]['lastCount'] = colors[colorKey]['currentCount'];
+//         colors[colorKey]['currentCount'] = 0;
+//     }
 
-    if (capture.pixels.length > 0) {
-        for (var i = 0; i < pixelCount; i += 4) {
-            var totalDiff;
-            var closestTargetColor;
-            var minColorPixelDiff = 0;
-            for (var colorKey in colors) {
-                var c = colors[colorKey].rgb;
-                currentPixelDiff = (
-                    Math.abs(capture.pixels[i] - c[0]) 
-                    + Math.abs(capture.pixels[i + 1] - c[1]) 
-                    + Math.abs(capture.pixels[i + 2] - c[2])
-                );
-                if (minColorPixelDiff == 0 || minColorPixelDiff > currentPixelDiff) {
-                    closestTargetColor = colorKey;
-                    minColorPixelDiff = currentPixelDiff;
-                };
-            }
-            if (first_capture == true) {
-                colors[closestTargetColor]['firstCount'] += 1;
-            }
-            else {
-                colors[closestTargetColor]['currentCount'] += 1;
-            }
-        }
-        first_capture = false;
-    }
-}
+//     if (capture.pixels.length > 0) {
+//         for (var i = 0; i < pixelCount; i += 4) {
+//             var totalDiff;
+//             var closestTargetColor;
+//             var minColorPixelDiff = 0;
+//             for (var colorKey in colors) {
+//                 var c = colors[colorKey].rgb;
+//                 currentPixelDiff = (
+//                     Math.abs(capture.pixels[i] - c[0]) 
+//                     + Math.abs(capture.pixels[i + 1] - c[1]) 
+//                     + Math.abs(capture.pixels[i + 2] - c[2])
+//                 );
+//                 if (minColorPixelDiff == 0 || minColorPixelDiff > currentPixelDiff) {
+//                     closestTargetColor = colorKey;
+//                     minColorPixelDiff = currentPixelDiff;
+//                 };
+//             }
+//             if (first_capture == true) {
+//                 colors[closestTargetColor]['firstCount'] += 1;
+//             }
+//             else {
+//                 colors[closestTargetColor]['currentCount'] += 1;
+//             }
+//         }
+//     }
+// }
 
 function playSounds(detectionDiffInt) {
-    // if diff between lastCount and currentCount is greater than specified int, start sound
     if (capture.pixels.length > 0) {
-        for (var colorKey in colors) {
-            var targetColor = colors[colorKey];
-            if ((targetColor["currentCount"] - targetColor["lastCount"] >= detectionDiffInt) 
-                && (targetColor["currentCount"] > targetColor["firstCount"])) {
+        for (let colortonesIndex in colortonesArray) {
+            let targetColor = colortonesArray[colortonesIndex];
+            if ((targetColor["currentCount"] - targetColor["lastCount"] >= detectionDiffInt)) {
                 targetColor["env"].play();
             }
         }
     }
 }
 
-function draw() {
-    var canvas = createCanvas(capture.width, capture.height);
-    background('pink');
-    var pixelD = pixelDensity();
-    pixelCount = 4 * (capture.width * pixelD) * (capture.height * pixelD);
+function setTone(note, arrayIndex) {
+    // instantiate oscillator
+    let env = new p5.Envelope();
+    env.setADSR(.001, .2, .2, .5);
+    env.setRange(0.4, 0.0);
 
+    let osc = new p5.Oscillator();
+    osc.setType("sine");
+
+    let freq = noteFreqObj[note];
+    osc.freq(freq);
+    osc.amp(env);
+    osc.start();
+
+    colortonesArray[arrayIndex]["frequency"] = freq;
+    colortonesArray[arrayIndex]["env"] = env;
+}
+
+function selectedNoteEvent(sel, itemIndex) {
+    let val = sel.value();
+    setTone(val, itemIndex);
+}
+
+function mousePressed() {
+    let colorToneObj = {
+        "color": get(mouseX, mouseY),
+        "lastCount": null,
+        "currentCount": null,
+        "frequency": null
+    };
+    if (colortonesArray.length < 12) {
+        colortonesArray.push(colorToneObj);
+
+        // TODO: populate within bounds of capture only
+        textAlign(CENTER);
+        let sel = createSelect();
+        sel.position(50 * (colortonesArray.length-1) + 10, capture.height + 65);
+        let sortedNotes = Object.keys(noteFreqObj).sort();
+        for (var keyIndex in sortedNotes) {
+            sel.option(sortedNotes[keyIndex]);
+        }
+        let partialSelectedNoteEvent = selectedNoteEvent.bind(null, sel, colortonesArray.length);
+        sel.changed(partialSelectedNoteEvent);
+    } else {
+        // TODO: change this to be displayed gui text
+        console.log("max of 12 allowed");
+    }
+}
+
+function draw() {
     image(capture, 0, 0, capture.width, capture.height);
 
+    // current color box
+    fill(get(mouseX, mouseY));
+    stroke("orange");
+    strokeWeight(4);
+    rect(25, 25, 50, 50);
+
+    // display all colortones
+    for (let colortonesIndex in colortonesArray) {
+        let currentColor = colortonesArray[colortonesIndex];
+        fill(currentColor["color"]);
+        let xPos = 50 * (int(colortonesIndex))+5;
+        rect(xPos, capture.height + 10, 40, 40);
+    }
+
     capture.loadPixels();
-    calculateClosestColors();
+    // calculateClosestColors();
     playSounds(detectionDiffInt);
 }
